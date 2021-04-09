d05d2baa8d3e13ebdf7e030409687633f6a678cb
Mariusz Kręt <mariuszkret1981@interia.pl>
2021-04-09 17:46:12 +0200

    Main, MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,20 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String mySon= "Syn jest dorosły";
+        System.out.println(mySon);
+        int numberBrothers = (3);
+        System.out.println(numberBrothers);
+        double myWeight= ( 80.3);
+        System.out.println(myWeight);
+        boolean myFirstcourse = (true);
+        System.out.println(myFirstcourse);
+        char lastLettersurname = 'T';
+        System.out.println(lastLettersurname);
+        int ageMyson= (11);
+        System.out.println(ageMyson);
+
+
+
+
+    }
+}
