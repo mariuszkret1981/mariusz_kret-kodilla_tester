3634c78d9bf9254d4156ef5047f9433797168fb8
Mariusz Kręt <mariuszkret1981@interia.pl>
2021-04-10 20:08:19 +0200

    leapYear

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,7 +1,13 @@
 public class FirstClass {                           // [1]
     public static void main(String[] args) {         // [2]
-       String example = "Hello From FirstClass";
-        System.out.println(example);
+        System.out.println("hello From FirstClass");
+
+        if (1>2){
+            System.out.println("Niesamowite! jeden jest większe od dwóch");
+
+        } else {
+            System.out.println(" Wszystko po staremu, jeden jest mniejsze niż dwa.");
+        }
 
     }
 }
---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,36 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2000;
+        int firstNumber = 4;
+        int secondNumber = 100;
+        int threeNumber = 400;
+        System.out.println(year);
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+        System.out.println(threeNumber);
+
+
+
+        if (year % firstNumber == 0 && year % secondNumber == 0 && year % threeNumber == 0) {
+            System.out.println("Rok jest przestepny");
+
+        }else {
+            System.out.println(" rok nie jest przestepny");
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
