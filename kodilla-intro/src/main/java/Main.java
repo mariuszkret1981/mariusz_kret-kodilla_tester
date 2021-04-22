74a2f7757fc2c0b16d2bb802c2bdc2d86aa4f76a
Mariusz Kręt <mariuszkret1981@interia.pl>
2021-04-22 20:00:03 +0200

    [GradesMain]

--- kodilla-intro/src/main/java/Main.java
+++ kodilla-intro/src/main/java/Main.java
@@ -1,11 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
-       Book book = new Book();
-       book.setTytul("Ogniem i mieczem");
-       book.displayTytul();
-       book.setAutor("Henryk Sienkiewicz");
-       book.displayAuthor();
+        Grades grades = new Grades();
+        grades.add(7);
+        grades.add(8);
+
+
+        int lastGrade = grades.getLastGrade();
+        System.out.println(lastGrade);
+        grades.add(15);
+        grades.add(133);
+        System.out.println(grades.getLastGrade());
+        System.out.println(grades.getAverage());
+
+        grades.add(3);
+        grades.add(3);
+        grades.add(3);
+        grades.add(3);
+        grades.add(3);
+        grades.add(3);
+        grades.add(7);
+        System.out.println(grades.getLastGrade());
+
+
 
 
     }
