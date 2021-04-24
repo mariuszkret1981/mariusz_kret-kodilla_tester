e209d80f64184e50f762cd9702e0e123fc73f53d
Mariusz Kręt <mariuszkret1981@interia.pl>
2021-04-24 13:30:29 +0200

    RandomNumbers,Main4

--- kodilla-intro/src/main/java/Main4.java
+++ kodilla-intro/src/main/java/Main4.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Main4 {
+public class Main4 {
+
+
+    public static void main(String[] args) {
+
+
+        RandomNumbers randomNumbers = new RandomNumbers();
+
+        randomNumbers.getRandom();
+        System.out.println(randomNumbers.getMin());
+        System.out.println(randomNumbers.getMax());
+    }
+
 }
--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Randomnumbers {
+import java.util.Random;
+
+public class RandomNumbers {
+    int min;
+    int max;
+
+    public void getRandom() {
+        Random random = new Random();
+        int sum = 0;
+        int max = 5000;
+        int first = random.nextInt(30);
+        this.min = first;
+        this.max = first;
+        sum = sum +first;
+        while (sum < max) {
+            int temp = random.nextInt(30);
+            sum = sum + temp;
+
+            if (temp < this.min) {
+                this.min = temp;
+            }
+            if (temp>this.max){
+                this.max = temp;
+            }
+            System.out.println("min: " +this.min);
+            System.out.println("max: " +this.max);
+            System.out.println("randomNumber: "+ temp);
+        }
+
+
+    }
+
+    public int getMin() {
+        return this.min;
+    }
+
+    public int getMax() {
+        return this.max;
+    }
+
 }
