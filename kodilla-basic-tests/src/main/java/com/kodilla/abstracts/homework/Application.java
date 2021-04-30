95fa872dbe7711bad52bdf535a699a1db006e017
Mariusz Kręt <mariuszkret1981@interia.pl>
2021-04-30 17:45:08 +0200

    Application

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Application.java
@@ -1,2 +1,16 @@
-package com.kodilla.abstracts.homework;public class Application {
+package com.kodilla.abstracts.homework;
+
+public class Application {
+    public static void main(String[] args) {
+        Square square = new Square();
+        square.getPatternForTheField();
+        square.getPatternForTheCircuit();
+        Rectangle rectangle = new Rectangle();
+        rectangle.getPatternForTheField();git add
+        rectangle.getPatternForTheCircuit();
+        Triangle triangle = new Triangle();
+        triangle.getPatternForTheField();
+        triangle.getPatternForTheCircuit();
+
+    }
 }
