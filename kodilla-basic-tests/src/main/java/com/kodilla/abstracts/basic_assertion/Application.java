22a538b45db038da761be21695fdb75bab293b8f
Mariusz Kręt <mariuszkret1981@interia.pl>
2021-05-01 15:37:24 +0200

    Application

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/basic_assertion/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/basic_assertion/Application.java
@@ -3,13 +3,14 @@ package com.kodilla.abstracts.basic_assertion;
 public class Application {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        int a = 4;
-        int squareResults = calculator.square(a);
-        boolean correct = ResultChecker.assertEquals(16,squareResults);
+        int a = 8;
+        int b = 4;
+        int subtractResults = calculator.subtract(a,b);
+        boolean correct = ResultChecker.assertEquals(4,subtractResults);
         if (correct) {
-            System.out.println("Metoda square działa poprawnie dla liczb " + ( a * a ) );
+            System.out.println("Metoda subtract działa poprawnie dla liczb " + ( a - b) );
         } else {
-            System.out.println("Metoda square nie działa poprawnie dla liczb " + ( a * a ) );
+            System.out.println("Metoda subtract nie działa poprawnie dla liczb " + ( a - b ) );
         }
     }
 }
